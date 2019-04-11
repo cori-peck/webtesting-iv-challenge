88a2f94f34a6ebc22658e4e31f0fcbc82213f8d5
cori-peck <cori.peck@ymail.com>
2019-04-11 17:04:40 -0500

    database made

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const server = require('./api/server.js');
+const knex = require('knex');
+const knexConfig = require('./knexfile');
+
+const db = knex(knexConfig.development);
 
 const port = process.env.PORT || 5050;
 server.listen(port, () => console.log(`*** Server is up and running on ${port} ***`));
